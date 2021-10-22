2bef9a4b8a3ac23258604532929ac55cff110e63
tonousa <tonousa@hotmail.com>
2021-10-22 11:46:31 -0500

    created initializer to seed db

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ namespace ContosoUniversity
                 {
                      var context = services.GetRequiredService<SchoolContext>();
                      context.Database.EnsureCreated();
-                     // DbInitializer.Initialize(context);
+                     DbInitializer.Initialize(context);
                 }
                 catch (Exception ex)
                 {
